c59643b73411f55e0ce74c26f60f569a5d44214a
teppeiOno <lemmte2moka@gmail.com>
2024-04-14 14:12:22 +0900

    emblaスライダー導入

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require('path');
 
 const nextConfig = {
   trailingSlash: true,
+  images: {
+    domains: ['storage.googleapis.com'],
+  },
 }
 
 module.exports = nextConfig
--- src/app/component/PickUp.tsx
+++ src/app/component/PickUp.tsx
@@ -1,4 +1,20 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react';
+import { DotButton, useDotButton } from './EmblaCarouselDotButton';
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons';
+
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType
+}
 
 // Article型の定義
 interface Article {
@@ -28,21 +44,82 @@ interface Props {
   obj: Article[];
 }
 
-const PickUp: React.FC<Props> = ({ obj }) => {
-  const pickUpItems = obj.filter(item => item.pickup);
-  console.log(pickUpItems);
+const PickUp: React.FC<Props> = ({ obj }, props) => {
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [pickUpItems, setPickUpItems] = useState<Article[]>([]);
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // 初回レンダー時にウィンドウの幅を取得
+    handleResize();
+
+    // ウィンドウのリサイズ時にイベントリスナーを設定
+    window.addEventListener('resize', handleResize);
+
+    // コンポーネントのアンマウント時にイベントリスナーをクリーンアップ
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    // ピックアップアイテムをフィルタリングして設定
+    const filteredPickUpItems = obj.filter(item => item.pickup);
+    setPickUpItems(filteredPickUpItems);
+  }, [obj]); // objが変更されたときのみ実行される
+  console.log(pickUpItems)
+
+  // PCとSPでheightとwidthの値を変えるための条件分岐
+  const height = windowWidth >= 961 ? 400 : 180;
+  const width = windowWidth >= 961 ? 900 : 400;
+  
   return (
     <section className='p-top-pickup'>
       <div className='l-container'>
-        <ul>
-          {pickUpItems.map(items => (
-            <li key={items._id}>
-              <p>{items.title}</p>
-            </li>
-          ))
-
-          }
-        </ul>
+        <div className='embla p-top-pickup__slider' ref={emblaRef}>
+          <ul className='p-top-pickup__list embla__container'>
+            {pickUpItems.map(items => (
+              <li className='p-top-pickup__item embla__slide' key={items._id}>
+                <Link href={'/blog/?page='+items._id+''} className="p-top-pickup__link">
+                  <p className='p-top-pickup__media'>
+                    <Image
+                      src={items.thumb.src}
+                      alt={items.thumb.altText}
+                      width={width}
+                      height={height}
+                      className="p-top-pickup__thumb"
+                      priority
+                    />
+                  </p>
+                  <p className='p-top-pickup__tag'><span className='p-top-pickup__tag-text'>{items.category}</span></p>
+                  <p className='p-top-pickup__title'>{items.title}</p>
+                </Link>
+              </li>
+            ))
+            }
+          </ul>
+        </div>
+        <div className="embla__controls">
+          <div className="embla__buttons">
+            <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+            <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+          </div>
+      </div>
       </div>
     </section>
   )
