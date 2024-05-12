512fd77ce4faf5df1245c340e6d8d8bd40a264bc
teppeiOno <lemmte2moka@gmail.com>
2024-05-12 21:32:29 +0900

    notionAPIのエンドポイント作成

--- src/pages/api/page.tsx
+++ src/pages/api/page.tsx
@@ -2,15 +2,27 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const token = 'OIfDDAlgRJZKkeZUbX0Jyot8H9fK6J2yOQEQvPEI8fI';
-    const response = await fetch('https://dot-blog.cdn.newt.so/v1/blog/post/', {
-      method: 'GET',
+    const notionVersion = '2022-06-28';
+    const endPoint = process.env.NOTION_API_ENDPOINT;
+    const token = process.env.NOTION_API_TOKEN;
+    if (!endPoint) {
+      throw new Error('NOTION_API_ENDPOINT is not defined in environment variables');
+    }
+    const response = await fetch(endPoint, {
+      method: 'POST',
       headers: {
-        'Authorization': 'Bearer ' + token,
+        'Authorization': `Bearer ${token}`,
+        'Notion-Version': notionVersion,
         'Content-Type': 'application/json', // 他のヘッダーも必要なら追加
       },
-    }).then(res => res.json())
-    res.status(200).json({ message: 'Hello from API', response });
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from Notion API');
+    }
+
+    const data = await response.json();
+
+    res.status(200).json({ message: 'Success', data });
   } catch (error) {
     res.status(500).json({ error: 'failed to load data' });
   }
