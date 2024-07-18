3b92cc192d14d598f4e774eef3b822d605c3be26
teppeiOno <lemmte2moka@gmail.com>
2024-07-18 23:29:17 +0900

    調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <Header />
         {children}
